6f9af828425fd4bf119220e6c056a5ed8afffbb5
Victor Otieno <victorotieno598@gmail.com>
2021-06-04 17:51:39 +0300

    Move inline css to external css file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
-    def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
-        redirect_to post_path(@post)
-    end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
 
-    private
+  private
 
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_one_attached :post_image
+  has_many :comments
+  has_one_attached :post_image
 
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
