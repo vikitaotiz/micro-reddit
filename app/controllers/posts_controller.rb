d48cc8e98cfb1a4c83b569a9a3aefdf77901730d
Victor Otieno <victorotieno598@gmail.com>
2021-06-04 15:38:27 +0300

    Add image upload using active storage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :post_image)
   end
 end
